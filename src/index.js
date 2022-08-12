a93dff8ed7e474ccebed3c0aa8c6e40f9022a382
Sergei Buiko <sergeibuiko1@gmail.com>
2022-08-12 11:57:07 +0300

    feat: add algorithm solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  
+  if (matrix === undefined || matrix === [] || matrix === null || matrix.length === 0) 
+  { return [] }
+  for (let i = 0; i < matrix.length; i += 2) {
+    matrix[i].reverse()   
+  }
+
+  let arr = matrix.map((item) => item.reverse())
+  let str = arr.join(',')
+  let arr2 = str.split(',')
+  let lengths = arr2.map(item => Number(item));
+  
+  //arr = arr.concat(matrix)
+  return lengths //.sort(function(a, b) { return a - b; });
 }
+ 
+
+
